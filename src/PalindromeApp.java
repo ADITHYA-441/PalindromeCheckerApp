010a93ec2907506e98e1b5b2bd47a2e9d7849574
ADITHYA-441 <adithyakarthikeya07@gmail.com>
2026-03-11 09:50:29 +0530

    Base application setup

---
+++ src/PalindromeApp.java
@@ -0,0 +1,18 @@
+public class PalindromeApp {
+    public static void main(String[] args) {
+
+        // Displaying welcome message and application details
+        System.out.println("=======================================");
+        System.out.println("      Welcome to Palindrome Checker App");
+        System.out.println("=======================================");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+        System.out.println("Author           : Developer");
+        System.out.println("---------------------------------------");
+        System.out.println("This application will check whether");
+        System.out.println("a given string is a palindrome or not.");
+        System.out.println("---------------------------------------");
+
+    }
+}
+
