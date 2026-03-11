48baf2dfb7847bfb7010910413f11178ec0cf1be
ADITHYA-441 <adithyakarthikeya07@gmail.com>
2026-03-11 10:38:21 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -1,7 +1,4 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class PalindromeApp {
     public static void main(String[] args) {
@@ -11,18 +8,16 @@ public class PalindromeApp {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> deque = new ArrayDeque<>();
 
         for (char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
+            deque.addLast(c);
         }
 
         boolean isPalindrome = true;
 
-        while (!queue.isEmpty()) {
-            if (!queue.remove().equals(stack.pop())) {
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 isPalindrome = false;
                 break;
             }
@@ -30,7 +25,6 @@ public class PalindromeApp {
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-
 
 
         scanner.close();
